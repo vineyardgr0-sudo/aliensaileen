73ed27cd2142439342b1390c2f06dfcf27bf263a
Aileen <mungyuri@Aileen-MacBook-Air.local>
2026-05-31 21:24:47 +0900

    fix: hardcode redirectTo URL

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -9,7 +9,7 @@ export default function AuthButton() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin + "/auth/callback",
+        redirectTo: "https://www.aliensaileenapp.xyz/auth/callback",
       },
     })
   }
