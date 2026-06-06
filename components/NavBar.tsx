8b2cd51dfea1ad057cfcc51c28a7439814930539
Aileen <mungyuri@Aileen-MacBook-Air.local>
2026-06-06 20:53:09 +0900

    fix: remove LoginButton from NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,5 @@
 'use client'
 
-import LoginButton from '@/components/LoginButton'
-
 export default function NavBar() {
-  return (
-    <div className="fixed top-3 right-5 z-50">
-      <LoginButton />
-    </div>
-  )
+  return null;
 }
